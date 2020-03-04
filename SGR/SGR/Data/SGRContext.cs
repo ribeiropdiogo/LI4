abb3b429f6597a01478d97b79ad2b10993e4d4a1
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2020-03-04 12:45:41 +0000

    ações Create Update Delete

---
+++ SGR/SGR/Data/SGRContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SGR.Models;
+
+namespace SGR.Data
+{
+    public class SGRContext : DbContext
+    {
+        public SGRContext(DbContextOptions<SGRContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Funcionário> Funcionários { get; set; }
+
+    }   
+}
+
---
+++ SGR/SGR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SGR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
