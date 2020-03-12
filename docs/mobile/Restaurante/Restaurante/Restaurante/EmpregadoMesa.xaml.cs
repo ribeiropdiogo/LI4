8691ef3c0263dea8febe8fc38be483addc71965a
priest110 <49480451+priest110@users.noreply.github.com>
2020-03-12 17:41:04 +0000

    Add files via upload

---
+++ docs/mobile/Restaurante/Restaurante/Restaurante/EmpregadoMesa.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Restaurante
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmpregadoMesa : ContentPage
+    {
+        public EmpregadoMesa()
+        {
+            InitializeComponent();
+        }
+
+        private void BotaoListaPedidos(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new ListaPedidos());
+        }
+
+        private void BotaoAdicionar(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new AdicionarPedido());
+        }
+
+        private void BotaoSair(object sender, EventArgs e)
+        {
+            DisplayAlert("Bom trabalho!", "Até à próxima", "Ok");
+            Navigation.PushModalAsync(new Menu());
+        }
+    }
+}
