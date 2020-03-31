2fcf4f4c6a58df89e8f766123dc5e7e30b4e9b4e
priest110 <49480451+priest110@users.noreply.github.com>
2020-03-31 17:07:42 +0100

    Add files via upload

---
+++ docs/mobile/Restaurante/Restaurante/Restaurante/Models/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Restaurante.Models
+{
+    public class Funcionario
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string morada { get; set; }
+        public string nome { get; set; }
+        public string cargo { get; set; }
+        public int idHorario { get; set; }
+        public int idGerente { get; set; }
+        public virtual ICollection<Pedido> Pedidos { get; set; }
+
+        public Funcionario(int id, string email, string password, DateTime dataNascimento, string morada, string nome, string cargo, int idHorario, int idGerente)
+        {
+            this.id = id;
+            this.email = email;
+            this.password = password;
+            this.dataNascimento = dataNascimento;
+            this.morada = morada;
+            this.nome = nome;
+            this.cargo = cargo;
+            this.idHorario = idHorario;
+            this.idGerente = idGerente;
+            Pedidos = new HashSet<Pedido>();
+        }
+
+        public static Funcionario Clone<Funcionario>(Funcionario source)
+        {
+            var serialized = JsonConvert.SerializeObject(source);
+            return JsonConvert.DeserializeObject<Funcionario>(serialized);
+        }
+
+    }
+}
---
+++ docs/mobile/Restaurante/Restaurante/Restaurante/Models/Mercadoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurante.Models
+{
+    class Mercadoria
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int marca { get; set; }
+        public int stock { get; set; }
+        public string observacoes { get; set; }
+        public int quantidade_min { get; set; }
+        public string embalagem { get; set; }
+        public Mercadoria(int id, string nome, int marca, int stock, string observacoes, int quantidade_min, string embalagem)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.marca = marca;
+            this.stock = stock;
+            this.observacoes = observacoes;
+            this.quantidade_min = quantidade_min;
+            this.embalagem = embalagem;
+
+        }
+    }
+}
