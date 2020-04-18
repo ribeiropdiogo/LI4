a8a7c1b54296a23172b44bd7959459e52f3485a3
priest110 <49480451+priest110@users.noreply.github.com>
2020-04-18 04:49:56 +0100

    Add files via upload

---
+++ docs/mobile/Restaurante/Restaurante/Restaurante/ViewModels/ErrorandEmpty/SomethingWentWrongPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Restaurante.Views.Forms;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace Restaurante.ViewModels.ErrorAndEmpty
+{
+    [Preserve(AllMembers = true)]
+    public class SomethingWentWrongPageViewModel : BaseViewModel
+    {
+        private string imagePath;
+        public SomethingWentWrongPageViewModel()
+        {
+            this.ImagePath = "SomethingWentWrong.svg";
+            this.TryAgainCommand = new Command(this.TryAgain);
+        }
+        public ICommand TryAgainCommand { get; set; }
+        public string ImagePath
+        {
+            get
+            {
+                return this.imagePath;
+            }
+            set
+            {
+                this.imagePath = value;
+            }
+        }
+        private void TryAgain(object obj)
+        {
+            App.Current.MainPage.Navigation.PushModalAsync(new LoginPage());
+        }
+    }
+}
